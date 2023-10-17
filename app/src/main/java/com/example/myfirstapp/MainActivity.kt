a8d222a00450fb2f30f80d502324b5c9d752f6f0
Andrii Krenevych <akrenevych@gmail.com>
2023-10-17 15:43:07 +0300

    Constraint

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button1)
+        textView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editTextText)
+
+        button.setOnClickListener {
+            val name = editText.text.toString()
+            textView.text = "Hello, $name!"
+        }
+
     }
 }
