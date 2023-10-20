32b13e7c28d1d65b6978cd0ef272224c0b41675e
Andrii Krenevych <akrenevych@gmail.com>
2023-10-20 11:33:28 +0300

    Click listener for text view

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,12 +11,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var button1: Button
     private lateinit var button2: Button
+    private lateinit var textView: TextView
+
 
 
     private inner class CustomOnClickListener : View.OnClickListener {
         override fun onClick(view: View?) {
             view?.let {
                 if (it is Button) {
+                    val toast = Toast.makeText(this@MainActivity, it.text.toString(), Toast.LENGTH_SHORT)
+                    toast.show()
+                }
+                if (it is TextView) {
                     val toast = Toast.makeText(this@MainActivity, it.text.toString(), Toast.LENGTH_SHORT)
                     toast.show()
                 }
@@ -33,9 +38,11 @@ class MainActivity : AppCompatActivity() {
 
         button1 = findViewById(R.id.button1)
         button2 = findViewById(R.id.button2)
+        textView = findViewById(R.id.textView1)
 
         button1.setOnClickListener(clickListener)
         button2.setOnClickListener(clickListener)
+        textView.setOnClickListener(clickListener)
 
     }
 }
