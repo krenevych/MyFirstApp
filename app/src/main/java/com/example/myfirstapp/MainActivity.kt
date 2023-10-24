d5aaf0696b33ae4e14efd3e3b632304675d3eab1
Andrii Krenevych <akrenevych@gmail.com>
2023-10-24 14:26:53 +0300

    Spinner has been finished

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -11,7 +12,18 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var imageView: ImageView
+//    https://developer.android.com/develop/ui/views/components/spinner
     private lateinit var spinner: Spinner
+
+
+    private val images = listOf(
+        R.drawable.cat_first,
+        R.drawable.cat_second,
+        R.drawable.cat_third,
+        R.drawable.cat_4,
+        R.drawable.cat_5,
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +41,16 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         spinner.onItemSelectedListener = this
     }
 
-    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        TODO("Not yet implemented")
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//        val item = parent?.adapter?.getItem(position)
+        val item = parent?.getItemAtPosition(position)
+        Log.d("XXXX", "selected $item")
+        if (item != null){
+            imageView.setImageResource(images[position])
+        }
     }
 
-    override fun onNothingSelected(p0: AdapterView<*>?) {
-        TODO("Not yet implemented")
+    override fun onNothingSelected(parent: AdapterView<*>?) {
     }
 
 
