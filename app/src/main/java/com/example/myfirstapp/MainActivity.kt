209892e0dd314ee610c7ab94613640536f88208d
Andrii Krenevych <akrenevych@gmail.com>
2023-10-24 13:06:06 +0300

    CheckBox

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.CheckBox
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var button1: Button
     private lateinit var imageView: ImageView
+    private lateinit var cat1: CheckBox
+    private lateinit var cat2: CheckBox
+    private lateinit var cat3: CheckBox
+
     private val images = listOf(
         R.drawable.cat_first,
         R.drawable.cat_second,
@@ -20,13 +24,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1 = findViewById(R.id.button1)
-        imageView = findViewById(R.id.imageView)
-        button1.setOnClickListener {
-            curImageIndex = (curImageIndex + 1) % images.size
-            val imageRes = images[curImageIndex]
-            imageView.setImageResource(imageRes)
+        imageView = findViewById<ImageView?>(R.id.imageView).apply {
+            visibility = View.INVISIBLE
         }
+        cat1 = findViewById(R.id.checkBoxCat1)
+        cat2 = findViewById(R.id.checkBoxCat2)
+        cat3 = findViewById(R.id.checkBoxCat3)
+
+        cat1.setOnClickListener {
+            if (cat1.isChecked){
+                imageView.visibility = View.VISIBLE
+                imageView.setImageResource(images[0])
+
+                cat2.isChecked = false
+                cat3.isChecked = false
+            }else {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+
+        cat2.setOnClickListener {
+            if (cat2.isChecked){
+                with(imageView){
+                    setImageResource(images[1])
+                    visibility = View.VISIBLE
+                }
+                cat1.isChecked = false
+                cat3.isChecked = false
+            } else {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+
+        cat3.setOnClickListener {
+            if (cat3.isChecked){
+                with(imageView){
+                    setImageResource(images[2])
+                    visibility = View.VISIBLE
+                }
+                cat1.isChecked = false
+                cat2.isChecked = false
+            }else {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+
     }
 
 }
