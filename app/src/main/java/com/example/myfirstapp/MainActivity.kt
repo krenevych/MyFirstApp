31089db440fb92a22563bb1cddfa373d596617a6
Andrii Krenevych <akrenevych@gmail.com>
2023-10-24 13:56:58 +0300

    Spinner

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,53 +1,41 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.CheckBox
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.ToggleButton
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var imageView: ImageView
-    private lateinit var toggleButton: ToggleButton
-
-
-    private val images = listOf(
-        R.drawable.cat_first,
-        R.drawable.cat_second,
-        R.drawable.cat_third,
-    )
+    private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageView = findViewById(R.id.imageView)
-        toggleButton = findViewById(R.id.cat_pusher)
-//        toggleButton.setOnClickListener{
-////            Log.d("XXXX", "toggle state: ${toggleButton.isChecked}")
-//            val state = toggleButton.isChecked
-//            if (state) {
-//                imageView.setImageResource(images[2])
-//            } else {
-//                imageView.setImageResource(images[0])
-//            }
-//        }
-
-
-        toggleButton.setOnCheckedChangeListener { button, state ->
-            if (state){
-                imageView.setImageResource(images[2])
-            } else {
-                imageView.setImageResource(images[0])
-            }
-        }
-
-
-
+        spinner = findViewById(R.id.catSpinner)
+        val spinnerAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.cats,
+            android.R.layout.simple_spinner_item
+        )
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = spinnerAdapter
+        spinner.onItemSelectedListener = this
     }
 
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
 
 
 }
