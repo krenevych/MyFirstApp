206bbc8fb3b3d16e9706bce1b6d93960d4de1fe5
Andrii Krenevych <akrenevych@gmail.com>
2023-10-24 13:31:24 +0300

    ToggleButton

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
-import android.view.View
-import android.view.View.OnClickListener
+import android.util.Log
 import android.widget.CheckBox
 import android.widget.ImageView
-import android.widget.RadioButton
+import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
-    private lateinit var cat1: RadioButton
-    private lateinit var cat2: RadioButton
-    private lateinit var cat3: RadioButton
+    private lateinit var toggleButton: ToggleButton
+
 
     private val images = listOf(
         R.drawable.cat_first,
@@ -26,23 +24,30 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         setContentView(R.layout.activity_main)
 
         imageView = findViewById(R.id.imageView)
-        cat1 = findViewById(R.id.cat1)
-        cat2 = findViewById(R.id.cat2)
-        cat3 = findViewById(R.id.cat3)
+        toggleButton = findViewById(R.id.cat_pusher)
+//        toggleButton.setOnClickListener{
+////            Log.d("XXXX", "toggle state: ${toggleButton.isChecked}")
+//            val state = toggleButton.isChecked
+//            if (state) {
+//                imageView.setImageResource(images[2])
+//            } else {
+//                imageView.setImageResource(images[0])
+//            }
+//        }
 
-        cat1.setOnClickListener(this)
-        cat2.setOnClickListener(this)
-        cat3.setOnClickListener(this)
 
-
-    }
-
-    override fun onClick(p0: View?) {
-        when {
-            cat1.isChecked -> imageView.setImageResource(images[0])
-            cat2.isChecked -> imageView.setImageResource(images[1])
-            cat3.isChecked -> imageView.setImageResource(images[2])
+        toggleButton.setOnCheckedChangeListener { button, state ->
+            if (state){
+                imageView.setImageResource(images[2])
+            } else {
+                imageView.setImageResource(images[0])
+            }
         }
+
+
+
     }
+
+
 
 }
