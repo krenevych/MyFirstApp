a8ec98e50848e3be9099a4a11d7ac4a8d3844fd0
Andrii Krenevych <krenevych@knu.ua>
2024-11-07 23:26:19 +0200

    ViewBinding for RecyclerView

--- app/src/main/java/com/example/myfirstapp/CustomAdapter.kt
+++ app/src/main/java/com/example/myfirstapp/CustomAdapter.kt
@@ -9,21 +9,23 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myfirstapp.databinding.CatItemBinding
 
 class CustomAdapter(
 //    private val context: Context,
-                    private val dataSet: List<ItemData>,
-):
+    private val dataSet: List<ItemData>,
+) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     /**
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder)
      */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageViewCat: ImageView = view.findViewById(R.id.imageViewCat)
-        val textViewCat: TextView = view.findViewById(R.id.nameCat)
-        val catCardView: CardView = view.findViewById(R.id.catCardView)
+    class ViewHolder(binding: CatItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        val imageViewCat: ImageView = binding.imageViewCat
+        val textViewCat: TextView = binding.nameCat
+        val catCardView: CardView = binding.catCardView
         val id = curId++
 
         init {
@@ -35,20 +37,20 @@ class CustomAdapter(
         var curId = 0
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.cat_item, parent, false)
-
-        Log.d("XXXXX", "onCreateViewHolder")
-
-        return ViewHolder(view)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding: CatItemBinding = CatItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount() = dataSet.size
 
-    override fun onBindViewHolder(holder: ViewHolder,
-                                  position: Int) {
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
         holder.imageViewCat.setImageResource(dataSet[position].resId)
         holder.textViewCat.text = dataSet[position].name
 //        holder.catCardView.setOnClickListener{
